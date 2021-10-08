1d13d41f9b4d130ad677571aa63528507885ecd6
ChiefORZ <seb.schaffernak@gmail.com>
2021-10-08 12:38:40 +0200

    Initialize Repository

---
+++ index.js
@@ -0,0 +1,27 @@
+const { json, send } = require('micro');
+const { match, test } = require('micro-match');
+const axios = require('axios');
+const parse = require('urlencoded-body-parser');
+
+module.exports = async (req, res) => {
+  try {
+    if (test('/ifttt/:key', req.url)) {
+      const { key } = match('/ifttt/:key', req.url);
+      const body = await parse(req);
+  
+      const iftttUrl = `https://maker.ifttt.com/trigger/heroku_deploy/with/key/${key}`;
+      const iftttBody = {
+        value1: `${body?.['app']} ${body?.['release']} was deployed to Heroku by ${body?.['user']}`,
+        value2: body?.['url'],
+        value3: `${body?.['app']} ${body?.['release']}`
+      }
+      await axios.post(iftttUrl, iftttBody);
+      return send(res, 200, 'ok');
+    } else {
+      return send(res, 404, 'not found');
+    }
+  } catch(err) {
+    console.error(err);
+    send(res, 500, 'internal server error');
+  }
+}
